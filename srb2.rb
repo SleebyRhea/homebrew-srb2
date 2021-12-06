463f56b0d59ed00040e16be562b8bc0723181294
Alison Wyatt <sleepyfugu@gmail.com>
2021-12-06 03:20:42 -0500

    Add caveat

--- srb2.rb
+++ srb2.rb
@@ -1,29 +1,30 @@
 class Srb2 < Formula
-  desc     "SRB2 2.2.9"
+  desc     "DOOM based 3D Sonic clone"
   homepage "https://www.srb2.org/"
   url      "https://github.com/STJr/SRB2/archive/refs/tags/SRB2_release_2.2.9.tar.gz"
   sha256   "5f7eeb08e90323e28cdcb02ad25c904eef25ce75316720609b995a1e4ffd154a"
   license  "GPL-2.0"
 
+  option "with-debug", "Set the target build type to Debug"
+
+  keg_only   "This formula builds a .app"
   depends_on "cmake" => :build
   depends_on "curl"
-  depends_on "zlib"
   depends_on "sdl2"
   depends_on "sdl2_mixer"
   depends_on "libpng"
   depends_on "libogg"
   depends_on "libvorbis"
   depends_on "libopenmpt"
+  depends_on :macos
   depends_on "game-music-emu"
   depends_on :xcode => "9.3"
+  depends_on "zlib"
 
   resource "srb2_assets" do
     url "https://github.com/STJr/SRB2/releases/download/SRB2_release_2.2.9/SRB2-v229-Full.zip"
     sha256 "48e644604bed81b5ce6b12ef23a2f4042d0118d0ba70f18c534dfe86ebe6f37d"
   end
-
-  option "build-debug", "Set the target build type to Debug"
-  option "app-dir", "Set the resting place for the SRB2.app symlink"
 
   # This prevents MacOS from loading external frameworks prior to libraries
   # installed by Homebrew. This (in particular) addresses issues with libpng
@@ -34,7 +35,6 @@ class Srb2 < Formula
   #   into the most recent release here: https://github.com/STJr/SRB2/releases
   #
   # Issue: https://git.do.srb2.org/STJr/SRB2
-  #patch :ISSUE_696
   patch :p1, 'diff --git a/CMakeLists.txt b/CMakeLists.txt.issue696
 index 5d2d4a7..24c2f5b 100644
 --- a/CMakeLists.txt
@@ -49,8 +49,8 @@ index 5d2d4a7..24c2f5b 100644
  set(CMAKE_RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/bin")'
 
   def install
-    build_type = "Release" unless build.with? "build-debug"
-    build_type = "Debug" if build.with? "build-debug"
+    build_type = "Release" unless build.with? "with-debug"
+    build_type = "Debug" if build.with? "with-debug"
     
     ENV.append "LIBRARY_PATH", "#{HOMEBREW_PREFIX}"
     ENV.append "MACOS_DEPLOYMENT_TARGET", "10.9"
@@ -76,8 +76,8 @@ index 5d2d4a7..24c2f5b 100644
 
     mkdir_p "#{buildpath}/build"
     chdir "#{buildpath}/build" do
-      system "cmake", "..", *args
-      system "make"
+      system 'cmake', '..', *args
+      system 'make'
 
       chdir "#{buildpath}/assets/installer" do
         assets = %w[
@@ -87,12 +87,22 @@ index 5d2d4a7..24c2f5b 100644
 
         Dir.foreach(".") do |f|
           next unless assets.include? f
-          next if f == "." or f == ".."
-          cp_r "#{f}", "#{buildpath}/build/bin/SRB2SDL2.app/Contents/Resources/"
+          
+          cp_r f.to_s, "#{buildpath}/build/bin/SRB2SDL2.app/Contents/Resources/"
         end
       end
 
       cp_r "#{buildpath}/build/bin/SRB2SDL2.app", "#{prefix}/SRB2.app"
     end
+  end
+
+  def caveats
+    <<~EOS
+      This formula creates a .app at #{prefix}/SRB2.app
+
+      You can run "open #{prefix}/SRB2.app", or symlink this application to
+      your ~/Applications directory:
+        ln -s '#{prefix}/SRB2.app' ~/Applications/SRB2.app
+    EOS
   end
 end
