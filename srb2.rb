9b50b63dad26d5d227bd12cb7ca3966b1fdcc9a9
Alison Wyatt <sleepyfugu@gmail.com>
2021-12-06 02:42:28 -0500

    Formula symlinks to /Applications

--- srb2.rb
+++ srb2.rb
@@ -93,6 +93,7 @@ index 5d2d4a7..24c2f5b 100644
       end
 
       cp_r "#{buildpath}/build/bin/SRB2SDL2.app", "#{prefix}/SRB2.app"
+      ln_s "#{prefix}/SRB2.app", "/Applications/SRB2.app"
     end
   end
 end
